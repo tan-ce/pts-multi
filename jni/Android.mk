5e311de9bf6b2482c008505d861039d18518b8f9
Tan Chee Eng <me@tan-ce.com>
2014-11-16 16:01:22 +0800

    Compile to position independent code for Lolipop

--- jni/Android.mk
+++ jni/Android.mk
@@ -2,7 +2,8 @@ LOCAL_PATH:=$(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := pts-multicall
-LOCAL_CFLAGS += -Wall
+LOCAL_CFLAGS += -Wall -fPIE
+LOCAL_LDFLAGS += -fPIE -pie
 LOCAL_C_INCLUDES := bionic
 
 LOCAL_SRC_FILES := main.c pts-shell.c pts-wrap.c pts-exec.c pts-daemon.c pts-passwd.c bcrypt.c blowfish.c helpers.c
